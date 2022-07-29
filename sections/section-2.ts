ac50fd7bda3dd8c2be0f5157ceee67c61679e4ae
Fernando Herrera <fernando.herrera85@gmail.com>
2022-07-29 14:45:14 -0400

    Fin sección 2

---
+++ sections/section-2.ts
@@ -0,0 +1,12 @@
+let numberOfLines = 0;
+
+export function printLineNumber() {
+    numberOfLines++;
+    console.log('Línea #', numberOfLines );
+}
+
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
+
